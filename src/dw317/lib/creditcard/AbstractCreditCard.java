3efebee94d71af9d41783968504adc868eb5f3b9
xavierberthiaume <xavierberthiaume@10.230.30.104>
2016-09-07 10:06:48 -0400

    Minor changes
    Changes to the AbstractCreditCard class

--- src/dw317/lib/creditcard/AbstractCreditCard.java
+++ src/dw317/lib/creditcard/AbstractCreditCard.java
@@ -1,5 +1,11 @@
 package dw317.lib.creditcard;
 
-public abstract class AbstractCreditCard {
-
+public abstract class AbstractCreditCard implements CreditCard{
+	private static final long serialVersionUID = 42031768871L;
+	private final CardType cardtype;
+	private final String number;
+	
+	public AbstractCreditCard(CardType cardtype, String number) throws IllegalArgumentException{
+		
+	}
 }
