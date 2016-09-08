47f1bf418d530662010481a84acf9bd7634abf9d
ehugh <ehugh@DESKTOP-TBFBK3L>
2016-09-08 13:38:35 -0400

    changed line 19 to show jon

--- src/dw317/lib/creditcard/AbstractCreditCard.java
+++ src/dw317/lib/creditcard/AbstractCreditCard.java
@@ -16,7 +16,7 @@ public abstract class AbstractCreditCard implements CreditCard{
 	 */
 	public boolean equals(Object object) {
 		
-		return true;
+		return true; // changed line 19 to show you how to fetch
 	}
 	
 	/**
