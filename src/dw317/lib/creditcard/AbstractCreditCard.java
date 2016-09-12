17d7d594203b1dde8ba0ad7467f2e5155a01f967
AminManai <AminManai@10.230.37.70>
2016-09-12 12:43:35 -0400

    changed abstractcreditcard to test

--- src/dw317/lib/creditcard/AbstractCreditCard.java
+++ src/dw317/lib/creditcard/AbstractCreditCard.java
@@ -16,7 +16,7 @@ public abstract class AbstractCreditCard implements CreditCard{
 	 */
 	public boolean equals(Object object) {
 		
-		return true; // changed line 19 to show you how to fetchssss
+		return true; // changed line 19 to show you how to fetch
 	
 	/**
 	 * @return number - The numbers of the credit card
