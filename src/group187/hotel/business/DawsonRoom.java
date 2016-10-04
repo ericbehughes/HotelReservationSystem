578a0ef42af673419cc3ada4c0e19bdb33d32781
Jepaz <jon.edepaz@gmail.com>
2016-10-04 10:59:31 -0700

    Fixed dawsonroom errors -Eric

--- src/group187/hotel/business/DawsonRoom.java
+++ src/group187/hotel/business/DawsonRoom.java
@@ -1,26 +1,27 @@
 package group187.hotel.business;
+
 import dw317.hotel.business.RoomType;
 import dw317.hotel.business.interfaces.Room;
 
 public class DawsonRoom implements Room {
 	private static final long serialVersionUID = 42031768871L;
 	private final int roomNumber;
-	private final RoomType roomType; 
-	
-	public DawsonRoom(int roomNumber,RoomType roomType){
-		
+	private final RoomType roomType;
+
+	public DawsonRoom(int roomNumber, RoomType roomType) {
+
 		if (!isValid(roomNumber))
 			throw new IllegalArgumentException();
-		this.roomNumber = roomNumber; 
-		this.roomType = roomType; 
+		this.roomNumber = roomNumber;
+		this.roomType = roomType;
 	}
-	
-	public boolean isValid(int roomNumber) throws IllegalArgumentException{
-		try{
-		
+
+	public boolean isValid(int roomNumber) throws IllegalArgumentException {
+		try {
+
 			int[] roomArr = new int[3];
 			// Converts the int to an array for easier digit extraction
-			for (int i = 2; i >= 0; i--){
+			for (int i = 2; i >= 0; i--) {
 				roomArr[i] = roomNumber % 10;
 				roomNumber = roomNumber / 10;
 			}
@@ -32,23 +33,25 @@ public class DawsonRoom implements Room {
 			if (roomArr[2] < 1 || roomArr[2] > 8)
 				throw new IllegalArgumentException("The room number can only end with 1 - 8");
 		}
-		
-		catch(IllegalArgumentException e){
+		//
+		catch (IllegalArgumentException e) {
 			System.out.print(e.getMessage());
 		}
-		
+
 		return true;
-		
+
 	}
+
 	@Override
-	public int getFloor(){
-		return ((this.roomNumber / 100) );
+	public int getFloor() {
+		return ((this.roomNumber / 100));
 	}
+
 	@Override
-	public int getNumber(){
+	public int getNumber() {
 		return (roomNumber % 10);
 	}
-	
+
 	@Override
 	public String toString() {
 		return (this.roomNumber + "*" + this.roomType);
@@ -56,21 +59,24 @@ public class DawsonRoom implements Room {
 
 	@Override
 	public int compareTo(Room o) {
-		
+
 		if (o == null)
 			throw new NullPointerException();
+		if (this == o)
+			return 0;
 		if (this.getFloor() < o.getFloor())
+			return -1;
+		else if (this.getFloor() > o.getFloor())
 			return 1;
-		if (this.getFloor() == o.getFloor()){
+		else if (this.getFloor() == o.getFloor()) {
 			if (this.getNumber() < o.getNumber())
-				return 1;
-			else if (this.getNumber() > o.getNumber())
 				return -1;
+			else if (this.getNumber() > o.getNumber())
+				return 1;
 			else
 				return 0;
 		}
-		if (this.getFloor() > o.getFloor())
-			return -1;
+
 		return 0;
 	}
 
