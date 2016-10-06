5c89fb71353798baa9ada5baddb71ed5c33f9966
Jepaz <jon.edepaz@gmail.com>
2016-10-06 09:52:04 -0700

    Created NameTest.java and renamed EmailTestClass to EmailTest

--- test/dw317/lib/test/EmailTest.java
+++ test/dw317/lib/test/EmailTest.java
@@ -1,10 +1,33 @@
 package dw317.lib.test;
 
+import dw317.lib.Email;
+
 public class EmailTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		Email email1 = new Email("zhu@-abc.com");
+		Email email2 = new Email("ahu@abc.com");
+		
+		String str1 = "zhu";
+		String str2 = "abc";
+		
+		int result2 = str1.compareToIgnoreCase(str2);
+		
+		
+		
+		int result = email1.compareTo(email2);
+		
+		email1 = new Email("zhu@abc.com");
+		email2 = new Email("A@bc.com");
+		
+		result = email1.compareTo(email2);
+		
+		email1 = new Email("zhu@ba.com");
+		email2 = new Email("A@bc.com");
+		
+		result = email1.compareTo(email2);
+		
 	}
-
 }
--- test/dw317/lib/test/NameTest.java
+++ test/dw317/lib/test/NameTest.java
@@ -1,10 +1,45 @@
 package dw317.lib.test;
 
+import dw317.lib.Name;
+
+/**
+ * @author Jonathan Depaz (Jepaz - Github)
+ *
+ */
 public class NameTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String firstName, lastName;
+		Name name;
+		
+		// Initialize variables (variables are correct format)
+		firstName = "Joe";
+		lastName = "Smith";
 
+		// Create object to test
+		name = new Name(firstName, lastName);
+		
+		// Check if object is correct
+		System.out.println("-----------Correct variables-----------");
+		System.out.println("First name:\t" + name.getFirstName());
+		System.out.println("Last name:\t" + name.getLastName());
+		System.out.println("Full name:\t" + name.toString());
+
+		// Initialize variables (variables are incorrect format)
+		firstName = "L";
+		lastName = "1-*&^#36734";
+		
+		// Create object to test bad var's 
+		try{
+			name = new Name(firstName, lastName);
+			System.out.println("-----------Incorrect variables-----------");
+			System.out.println("First name:\t" + name.getFirstName());
+			System.out.println("Last name:\t" + name.getLastName());
+			System.out.println("Full name:\t" + name.toString());
+		}
+		catch (IllegalArgumentException e){
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
