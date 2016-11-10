34331c9f303d81e3ff9cf546e88d9f351d609721
ehugh <ehugh@DESKTOP-6D85Q1O>
2016-11-10 18:37:43 -0500

    reservationlistdb

---
+++ src/group187/hotel/data/ReservationListDB.java
@@ -0,0 +1,75 @@
+package group187.hotel.data;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import dw317.hotel.business.RoomType;
+import dw317.hotel.business.interfaces.Customer;
+import dw317.hotel.business.interfaces.HotelFactory;
+import dw317.hotel.business.interfaces.Reservation;
+import dw317.hotel.business.interfaces.Room;
+import dw317.hotel.data.DuplicateReservationException;
+import dw317.hotel.data.NonExistingReservationException;
+import dw317.hotel.data.interfaces.ListPersistenceObject;
+import dw317.hotel.data.interfaces.ReservationDAO;
+
+public class ReservationListDB implements ReservationDAO {
+	
+	private List<Reservation> database;
+	private List<Room> allRooms;
+	private final ListPersistenceObject listPersistenceObject;
+	private final HotelFactory factory;
+	public ReservationListDB (ListPersistenceObject listPersistenceObject)
+	{
+		this.listPersistenceObject = listPersistenceObject;
+	}
+	public ReservationListDB (ListPersistenceObject listPersistenceObject,
+	HotelFactory factory)
+	{
+		this.listPersistenceObject = listPersistenceObject;
+		this.factory = factory;
+	}
+
+	@Override
+	public void add(Reservation reserv) throws DuplicateReservationException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void disconnect() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public List<Reservation> getReservations(Customer cust) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void cancel(Reservation reserv) throws NonExistingReservationException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public List<Room> getReservedRooms(LocalDate checkin, LocalDate checkout) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Room> getFreeRooms(LocalDate checkin, LocalDate checkout) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Room> getFreeRooms(LocalDate checkin, LocalDate checkout, RoomType roomType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void clearAllPast() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
