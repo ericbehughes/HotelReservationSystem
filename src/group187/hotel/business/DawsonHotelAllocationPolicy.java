9bde1836e35a2dc645e521445b92a34281b9b5a4
Jepaz <Jepaz>
2016-11-18 15:15:04 -0500

    Finished Phase 1 of PartIV HotelAllocationPolicy

--- src/group187/hotel/business/DawsonHotelAllocationPolicy.java
+++ src/group187/hotel/business/DawsonHotelAllocationPolicy.java
@@ -2,6 +2,7 @@ package group187.hotel.business;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -19,18 +20,41 @@ public class DawsonHotelAllocationPolicy implements AllocationPolicy {
 		this.reservationDAO = reservationDAO;
 	}
 
-
 	@Override
 	public Optional<Room> getAvailableRoom(LocalDate checkin, LocalDate checkout, RoomType roomtype) {
-		List<Room> freeRooms = reservationDAO.getFreeRooms(checkin, checkout, roomtype);
-		List<Integer> multipleFloors = new ArrayList<>();
-		int[] numFreeRooms = { 0, 0, 0, 0, 0, 0, 0, 0 };
-		int floorNum, highestNum = numFreeRooms[0], floor = 0, selectedRoom = 0;
-		// Find the total number of free rooms per floor
+		/** Var explanations
+		 *  freeRooms: A list of all the rooms available for reservation
+		 *  multipleFloors: In the case where we have multiple floors with the same # of free rooms
+		 *  				this list, which uses Arrays.asList to create a list with a fixed size, will represent the different floors
+		 *  				where floor = index + 1. So index 0 = floor 1, index 1 = floor 2, and so on. The point of this is to be able
+		 *  				find the lowest floor with the most amount of free rooms, just grab the first index that is not 0.
+		 *  
+		 *  numFreeRooms: This array indicates how many free rooms per floor there are where index + 1 = floor (0 = floor 1). 
+		 *  			  The value at index 0 will indicate the # of free rooms on floor 1.
+		 *  
+		 *  floorNum: Temporary var used to hold a floor number.
+		 *  
+		 *  highestNum: Used when iterating through the numFreeRooms array to find the highest number.
+		 *  
+		 *  floor: Which Floor to use when choosing the room to reserve.
+		 *  
+		 *  selectedRoom: The room to give to the customer.
+		 *  
+		 *  start: The floor to start searching on, based on the roomtype
+		 *  
+		 *  end: Thw floor to stop searching on, based on the roomtype
+		 */
+		List<Room> freeRooms = reservationDAO.getFreeRooms(checkin, checkout, roomtype); 
+		List<Integer> multipleFloors = Arrays.asList(new Integer[8]); 
+		int[] numFreeRooms = { 0, 0, 0, 0, 0, 0, 0, 0 }; 
+		int floorNum, highestNum = numFreeRooms[0], floor = 0, selectedRoom = 0, start = 0, end = 0;
+		Boolean matches = false;
+		
+		// Find the total number of free rooms per floor, read explanation on numFreeRooms above.
 		for (int i = 0; i < freeRooms.size(); i++) {
-			DawsonRoom temp = (DawsonRoom) freeRooms.get(i);
-			floorNum = temp.getFloor();
-			switch (floorNum) {
+			DawsonRoom temp = (DawsonRoom)freeRooms.get(i); // Get the room object at index
+			floorNum = temp.getFloor(); // Get the floor number
+			switch (floorNum) { 
 			case 1:
 				numFreeRooms[0] = numFreeRooms[0] + 1;
 				break;
@@ -67,49 +91,73 @@ public class DawsonHotelAllocationPolicy implements AllocationPolicy {
 				break;
 			}
 		}
-
-		// Find the highest number of free rooms in the array
-		// and the floor containing them;
-		for (int i = 0; i < numFreeRooms.length; i++) {
-			if (numFreeRooms[i] > highestNum){
-				highestNum = numFreeRooms[i];
-				floor = i + 1;
-			}
-			// If two or more floors have the same # of free rooms
-			// Add them both to a list to decide which one to choose
-			// later on
-			else if (numFreeRooms[i] == highestNum){
-				multipleFloors.add(i, numFreeRooms[i]);
-				multipleFloors.add(floor, highestNum);
-				highestNum = numFreeRooms[i];
-				floor = i + 1;
-			}
-		}		
-		if (multipleFloors.size() <= 0)
-			selectedRoom = pickRandomRoom(floor, freeRooms);
 		
-		else{
-			for (int i = 0; i < multipleFloors.size(); i++){
-				if (multipleFloors.get(i) > 0){
-					floor = i + 1;
-					break;
-				}
-			}
-			selectedRoom = pickRandomRoom(floor, freeRooms);
+		if (roomtype.equals(RoomType.NORMAL)){
+			start = 0;
+			end = 5;
 		}
 		
+		else if (roomtype.equals(RoomType.SUITE)){
+			start = 5;
+			end = 7;
+		}
+		
+		else if (roomtype.equals(RoomType.PENTHOUSE)){
+			start = 7;
+			end = 8;
+		}
+		/**
+		 * Find the floor with the highest number of free rooms
+		 */
+		for (; start < end; start++) {
+			if (numFreeRooms[start] > highestNum){
+				highestNum = numFreeRooms[start];
+				floor = start + 1;
+			}
+		
+			/**
+			 * If two or more floors have the same number of
+			 * free rooms, boolean matched = true and break out of loop.
+			
+			else if (numFreeRooms[i] == highestNum){
+				matches = true;
+			}
+			 */
+		}
+		
+		selectedRoom = pickRandomRoom(floor, freeRooms, roomtype);
 		DawsonRoom chosenRoom = new DawsonRoom(selectedRoom, roomtype);
 		return Optional.of((Room)chosenRoom);
 	}
 	
-	private int pickRandomRoom(int floor, List<Room> freeRooms){
-		Random randomFloor = new Random();
-		int room = randomFloor.nextInt((8 - 1) + 1) + 1;
-		if (freeRooms.contains((floor*100) + room))
+	/**
+	 * Randomly chooses one of the free rooms on the floor given in the parameter list
+	 * If the randomly chosen room is not available for reservation, the method will call itself
+	 * and repeat until it finds a room to use.
+	 * 
+	 * @param floor: The floor to chose the room from.
+	 * @param freeRooms: The list containing all the free rooms.
+	 * @param roomtype: The room type for the room.
+	 * @return room: The room number.
+	 */
+	private int pickRandomRoom(int floor, List<Room> freeRooms, RoomType roomtype){
+		/** Var explanation
+		 * random: Create a random object to generate a number between 1 and 8.
+		 * room: The randomly chosen room.
+		 * temp: A temporary object referencing DawsonRoom so that we can properly set a room number
+		 * checkRoom: A cast of DawsonRoom to Room so that we can check the list of free rooms
+		 * 			  for the same room.
+		 */
+		
+		Random random = new Random();
+		int room = random.nextInt((8 - 1) + 1) + 1;
+		DawsonRoom temp = new DawsonRoom((floor*100) + room, roomtype);
+		Room checkRoom = (Room)temp;
+		if (freeRooms.contains(checkRoom))
 			room = (floor*100) + room;
 			
 		else 
-			pickRandomRoom(floor, freeRooms);
+			pickRandomRoom(floor, freeRooms, roomtype);
 		
 		return room;
 	}
---
+++ test/group187/hotel/business/DawsonHotelAllocationPolicyTest.java
@@ -0,0 +1,53 @@
+package group187.hotel.business;
+
+import java.util.Optional;
+
+import dw317.hotel.business.RoomType;
+import dw317.hotel.business.interfaces.Reservation;
+import dw317.hotel.business.interfaces.Room;
+import dw317.lib.Email;
+import dw317.lib.Name;
+import dw317.lib.creditcard.Amex;
+import dw317.lib.creditcard.CreditCard;
+import group187.hotel.data.ReservationListDB;
+import group187.hotel.data.SequentialTextFileList;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+public class DawsonHotelAllocationPolicyTest {
+	public static void main(String[] args) {
+		// Set file paths
+		String roomFilename = "datafiles/database/rooms.txt";
+		String customerFilename = "datafiles/database/customers.txt";
+		String reservationFilename = "datafiles/database/reservations.txt";
+		
+		// Create database and factory object
+		SequentialTextFileList obj = new SequentialTextFileList(roomFilename, customerFilename, reservationFilename);
+		DawsonHotelFactory factory = DawsonHotelFactory.DAWSON;
+		ReservationListDB reservdb = new ReservationListDB(obj);
+		
+		// Create the customer and reservation objects
+		List<Room> freeRooms = new ArrayList<>();
+		DawsonRoom rm1 = new DawsonRoom(101, RoomType.NORMAL);
+		Email email1 = new Email("zhu@abcc.com");
+		Name name = new Name("test", "hughes");
+		Optional<CreditCard> cardTest1 = Optional.of(new Amex("374616906032009"));
+		DawsonCustomer customer1 = new DawsonCustomer(name.getFirstName(), name.getLastName(), email1, cardTest1);
+		DawsonReservation r1 = new DawsonReservation(customer1, rm1, 2015, 9, 1, 2016, 9, 9);
+		
+		// Get checkin and checkout dates
+		LocalDate checkin = r1.getCheckInDate();
+		LocalDate checkout = r1.getCheckOutDate();
+		
+		// Get all free rooms within checkin and checkout date for the given room type
+		freeRooms = reservdb.getFreeRooms(checkin, checkout, RoomType.NORMAL);
+		
+		// Begin test
+		DawsonHotelAllocationPolicy allocationPolicy = new DawsonHotelAllocationPolicy(reservdb);
+		Optional<Room> freeRoom = allocationPolicy.getAvailableRoom(checkin, checkout, RoomType.NORMAL);
+		System.out.println(freeRoom.toString());
+		
+	}
+
+}
